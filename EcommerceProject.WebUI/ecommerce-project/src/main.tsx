c36af8a82a030d491addc4aaea6eddb1fb21ad46
mickolapuz <mickolapuz19@gmail.com>
2025-04-16 00:07:37 +0800

    Changed layout styles

--- EcommerceProject.WebUI/ecommerce-project/src/main.tsx
+++ EcommerceProject.WebUI/ecommerce-project/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./main.css";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./styles/material.ts";
 
